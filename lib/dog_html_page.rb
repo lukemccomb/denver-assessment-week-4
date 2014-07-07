e9e474ef8d10469b9d8b6abd9f3b73866d9703a1
Luke McComb <lucasmccomb@gmail.com>
2014-07-06 20:25:04 -0600

    revisted this and realized what I wasn't seeing - got all tests to pass

--- lib/dog_html_page.rb
+++ lib/dog_html_page.rb
@@ -1,13 +1,30 @@
 class DogHTMLPage
+
     def initialize(names)
       @names = names
     end
 
     def title
-      title_h1 = ""
-      @names.select do |name, dog|
-        title_h1 << "#{dog}"
-        "<h1>" + title_h1 + "</h1>"
+      title_h1 = []
+      @names.select do |dog|
+        title_h1 << dog
+      end
+      if title_h1.length > 0
+      "<h1>There are #{title_h1.length.to_s} dogs</h1>"
+      else
+      "<h1>There are no dogs</h1>"
+      end
+    end
+
+    def list
+      list_of_dogs = "<ul>"
+      @names.each do |dog|
+        list_of_dogs << "<li>#{dog[:name]}</li>"
+      end
+      if list_of_dogs != "<ul>"
+        list_of_dogs << "</ul>"
+      else
+        ""
       end
     end
 end
